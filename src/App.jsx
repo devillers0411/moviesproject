6cee42bc01568ffc405fd67908c0206efd3d2037
devillers <devillers0411@gmail.com>
2025-12-29 23:44:14 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AppProvider } from './context/AppContext';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import Series from './pages/Series';
+import Search from './pages/Search';
+import MovieDetail from './pages/MovieDetail';
+import MyList from './pages/MyList';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Hello world</h1>
-  )
+    <Router>
+      <AppProvider>
+        <div className="app">
+          <Header />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/movies" element={<Movies />} />
+              <Route path="/series" element={<Series />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/movie/:id" element={<MovieDetail />} />
+              <Route path="/mylist" element={<MyList />} />
+            </Routes>
+          </main>
+        </div>
+      </AppProvider>
+    </Router>
+  );
 }
 
-export default App
+export default App;
